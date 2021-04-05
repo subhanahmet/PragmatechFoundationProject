92eb70ec3c1b2d063f590ba516b616b28d456420
subhanahmet <subhan.ahmedov@gmail.com>
2021-04-05 16:18:10 +0400

    Layihənin 3 səhifəsi əlavə olundu

--- project/main.js
+++ project/main.js
@@ -1,3 +1,7 @@
+// Div class ile
+// ----------------------------------------
+
+
 let box = document.querySelector('.slide-content')
 
 poz = 0
@@ -8,11 +12,49 @@ function test() {
 	poz += -1000
 
 	if (poz < -2000) {
+		poz = 0
 		box.style.transform = "translateX(0px)"
+		// box.style.transform = `translateX(${poz + 3000}px)`
+		// box.style.transform = `translateX(${poz - 1000}px)`
+		// poz -= 1000
 	}
 }
 
-setInterval('test()', 2000)
+// setInterval('test()', 2000)
+
+// Settime
+// ----------------------------------------
+
+// Set the date we're counting down to
+var countDownDate = new Date("May 5, 2021 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
 
 
 // Buton ile sağa sola kaydırma
@@ -82,9 +124,3 @@ setInterval('test()', 2000)
 
 // prevBtn.disabled = true
 // nextBtn.disabled = false
-
-
-
-
-
-
